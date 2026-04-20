8d00fb4326da1b668693c5962a2d0d6db33a6da4
bellafaulk <wva7@txstate.edu>
2026-04-20 15:25:12 -0500

    implemented simple face-data perceptron using dummy data, along with epochs and a way to test the model

--- src/models/perceptron.py
+++ src/models/perceptron.py
@@ -1,10 +1,48 @@
-# perceptron implementation
+# perceptron implementation: using simplified formula f(x) = wx + b
 # trains a model using weight updates, used for digit and face classification
 
-def train(X, y): {
-    pass
-}
+# model object with initialized weights and the Perceptron algorithm for reuse
+class Perceptron: 
 
-def predict(X): {
-    pass
-}
+    # sets up class weights for algorithm
+    def __init__(self):
+        self.weights = [0, 0, 0, 0] # needs to be random intialization
+        self.bias = 0
+    
+
+    # predicts and updates weights if it does not equal ground truth (y)
+    def train(self, X, y):
+        
+        for i in range(len(X)):
+            x = X[i]
+            actual = y[i]
+            prediction = self.predict(x)
+
+            # adjust weights towards actual answer if the prediction is incorrect
+
+            if prediction != actual:
+                if actual == 1:
+                    for j in range(len(x)):
+                        self.weights[j] = self.weights[j] + x[j]
+                    self.bias += 1
+                else:
+                    for j in range(len(x)):
+                        self.weights[j] = self.weights[j] - x[j]
+                    self.bias -= 1
+
+
+    # computes a score and defines it as true or false
+    def predict(self, x):
+        score = 0
+        for j in range(len(x)):
+            score += self.weights[j] * x[j]
+        score += self.bias
+        
+        if score >= 0:
+            score = 1   # true
+        else:
+            score = 0   # false
+
+        return score
+
+    
---
+++ src/models/test_perceptron.py
@@ -0,0 +1,37 @@
+from perceptron import Perceptron
+
+def main():
+    # defining dummy feature data (X) and labels (y)
+    # one image (2x2, 1 = "+ or #", 0 = whitespace)
+    X = [
+        [1, 1, 1, 0],
+        [1, 0, 1, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 1],
+    ]
+
+    # ground truth labels - 1 = face, 0 = not face
+    y = [1, 1, 0, 0]
+
+    model = Perceptron()
+    epochs = 10
+    
+
+    for epoch in range(epochs):
+        print("training model...")
+        print(f"\nEpoch: {epoch} out of {epochs}")
+        model.train(X, y)
+        predictions = []
+
+        for i in range(len(X)):
+            x = X[i]
+
+            predictions.append(model.predict(x))
+
+            print(f"\npredictions: {predictions}")
+            print(f"\nInput: {X[i]}, Predicted: {predictions[i]}, Actual: {y[i]}")
+
+        
+    
+if __name__ == "__main__":
+    main()
